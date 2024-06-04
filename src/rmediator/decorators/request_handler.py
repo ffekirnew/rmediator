91fe02e4a6635f75a03b8da8ab9891dcdc330a82
ffekirnew <ffekirnew0808@gmail.com>
2024-06-04 14:02:19 +0300

    feat: Added more checks on the decorators

--- src/rmediator/decorators/request_handler.py
+++ src/rmediator/decorators/request_handler.py
@@ -1,13 +1,4 @@
-from src.rmediator import Mediator
-from src.rmediator.interfaces import RequestHandlerInterface
-
-
 def request_handler(cls):
-    if not issubclass(cls, RequestHandlerInterface):
-        raise ValueError(
-            f"Request handler class cannot be registered, {cls} must be a subclass of RequestHandlerInterface."
-        )
-
     if "request" not in cls.__dict__:
         raise ValueError(
             f"Request handler class cannot be registered, {cls} must contain attribute 'request' to signify the reequest it handles."
@@ -16,6 +7,11 @@ def request_handler(cls):
     if "response" not in cls.__dict__:
         raise ValueError(
             f"Request handler class cannot be registered, {cls} must contain attribute 'response' to signify the response it sends after handling request."
+        )
+
+    if "handle" not in cls.__dict__:
+        raise ValueError(
+            f"Request handler class cannot be registered, {cls} must contain method 'handle' that handles the request."
         )
 
     if cls.request is None:  # type: ignore
@@ -33,9 +29,6 @@ def request_handler(cls):
             f"Request handler class cannot be registered, {cls} handle method must have return type of {cls.response}."  # type: ignore
         )
 
-    try:
-        Mediator().register_handler(cls)
-    except ValueError as e:
-        raise e
+    cls.rmediator_decorated_request_handler = True
 
     return cls
